a8db5201feaa7baf46c67519b29897f74f2c88b6
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-10 15:34:03 +0200

    Learn ngStyle and ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName ='Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor(){
     setTimeout(()=>{this.allowNewServer = true
@@ -22,7 +23,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+
   }
   onUpdateServerName(event){
     this.serverName = (<HTMLInputElement>event.target).value;
