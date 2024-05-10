59132cb1d4bd3ad7a2ff0b34e1bb68b8b4a54849
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-10 11:55:00 +0200

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
   constructor(){
     setTimeout(()=>{this.allowNewServer = true
     },2000);
   }
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
   }
 }
