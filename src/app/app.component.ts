2a0b8ab6a7c6473149bb7a55da588c21a0831af5
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-10 10:34:53 +0200

    templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h3{
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   name = 'András';
