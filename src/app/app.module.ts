808ecbf3580dec740c13d3880f20f9b5625cd4a1
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-10 09:23:07 +0200

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ServerComponent} from "./server/server.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
