01b9acad461dfb494edad1f457176fa576ba0d14
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-14 14:30:37 +0200

    declare new model

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(){
+  }
+}
