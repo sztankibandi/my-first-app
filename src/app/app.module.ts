97ee737eaf7e835cedc7c8a4397c4c9125faf269
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-14 11:03:16 +0200

    start new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { SuccessalertComponent } from './successalert/successalert.component';
-import {WarningAlert} from "./WarningAlert/warningalert.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessalertComponent,
-    WarningAlert
   ],
   imports: [
     BrowserModule,
