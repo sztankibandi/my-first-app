bf34edc646035463da4f7930782136eb6871b150
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-17 09:17:57 +0200

    assignment done

--- src/app/odd/odd.component.ts
+++ src/app/odd/odd.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './odd.component.css'
 })
 export class OddComponent {
+  @Input() number: number;
+
 
 }
