10b11d889dd57ff16f4d7e2ba04621732824cb4a
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-21 14:44:54 +0200

    start routing section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {LoggingService} from "./logging.service";
-import {AccountsService} from "./accounts.services";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  accounts: {name: string, status: string}[] = [];
-
-  constructor(private accountsService: AccountsService) {}
-
-  ngOnInit(): void {
-    this.accounts = this.accountsService.accounts;
-  }
+export class AppComponent {
 }
-
