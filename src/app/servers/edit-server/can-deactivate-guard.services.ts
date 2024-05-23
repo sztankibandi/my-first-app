0b34fe6d3282e985d87aa1e97160163072af9b58
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-23 14:12:37 +0200

    finished canDeactivate

---
+++ src/app/servers/edit-server/can-deactivate-guard.services.ts
@@ -0,0 +1,14 @@
+import {Observable} from "rxjs";
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from "@angular/router";
+
+export interface CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+
+}
+
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+  canDeactivate(component: CanComponentDeactivate, currentRoute: ActivatedRouteSnapshot,
+                nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return component.canDeactivate();
+  }
+}
