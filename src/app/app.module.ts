c2c3a7c0207182543210890eaa81adb2ad1bb7e7
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-23 13:40:53 +0200

    use canActivateChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AuthService} from "./auth.service";
+import {AuthGuard} from "./auth-guard.service";
 
-const appRoutes: Routes = [
-  { path: '' , component: HomeComponent },
-  { path: 'users' , component: UsersComponent, children: [
-      { path: ':id/:name' , component: UserComponent }
-    ] },
-  { path: 'servers' , component: ServersComponent, children: [
-      { path: ':id' , component: ServerComponent },
-      { path: ':id/edit' , component: EditServerComponent }
-    ] }
-];
 
 @NgModule({
   declarations: [
@@ -32,14 +25,15 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule,
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
