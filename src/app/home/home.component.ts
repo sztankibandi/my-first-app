a6ccad9224b0591b3844f7fd41755a796d4adecc
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-24 11:04:33 +0200

    start section 17-observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,21 +7,9 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  onLoadServer(id: number) {
-    // complex calculation
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
-  }
-
-  onLogin() {
-    this.authService.login();
-  }
-
-  onLogout() {
-    this.authService.logout();
-  }
 }
