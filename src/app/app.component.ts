efbc4f9789a26dc202e198821c35484d20621cc5
andras.sztankovics <andras.sztankovics@barre.hu>
2024-05-28 09:57:00 +0200

    TD: accessing the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { UserService } from './user.service';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
 
-  constructor(private userService: UserService) {
+  suggestUserName():void {
+    const suggestedName = 'Superuser';
   }
 
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
+  // onSubmit(form: NgForm):void {
+  //   console.log(form);
+  // }
+  onSubmit():void{
+    console.log(this.signupForm);
   }
 }
