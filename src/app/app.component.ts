a5ec5663e4473d5c49cfa87818310c0fbbed8abe
Sztankovics András <sztankibandi@gmail.com>
2024-05-30 12:58:03 +0200

    outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {Post} from "./post.module";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts:Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -48,7 +48,7 @@ export class AppComponent implements OnInit {
       }))
       .subscribe((posts)=> {
 
-      console.log(posts);
+      this.loadedPosts = posts;
     });
   }
 }
