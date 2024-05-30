98cf60b8cffc94b82aa5298c1288d6c00290b05b
Sztankovics András <sztankibandi@gmail.com>
2024-05-30 14:34:28 +0200

    adding query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
@@ -29,11 +29,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-7f223-default-rtdb.firebaseio.com/posts.json',
         {
           headers: new HttpHeaders({"Custom-Header": 'Hello'}),
+          params: searchParams
         }
       )
       .pipe(
