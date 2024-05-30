228a4a83a100367086edc5c860b8e27fd4bf2677
Sztankovics András <sztankibandi@gmail.com>
2024-05-30 09:19:20 +0200

    start http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,28 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion:string = 'pet';
-  answer:string = '';
-  genders:string[] = ['male', 'female'];
-  user:{username:string, email:string, secretQuestion:string, answer:string, gender:string}   = {
-    username:'',
-    email:'',
-    secretQuestion:'',
-    answer:'',
-    gender:''
-  };
-  submitted:boolean = false;
+export class AppComponent implements OnInit {
+  loadedPosts = [];
 
-  suggestUserName():void {
-    const suggestedName:string = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: '',
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male',
-    // });
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-      }
-    });
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
   }
 
-  // onSubmit(form: NgForm):void {
-  //   console.log(form);
-  // }
-  onSubmit():void{
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.username;
-    this.user.secretQuestion = this.signupForm.form.value.secret;
-    this.user.answer = this.signupForm.form.value.questionAnswer;
-    this.user.gender = this.signupForm.form.value.gender;
+  onFetchPosts() {
+    // Send Http request
+  }
 
-    this.signupForm.reset();
-
+  onClearPosts() {
+    // Send Http request
   }
 }
