198cfad0cf4be3e504a600c760cec7c806b30cd4
Sztankovics András <sztankibandi@gmail.com>
2024-05-30 16:41:25 +0200

    observing different types os responses

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
-import { Subject, throwError } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
 
-import { Post } from './post.model';
+import {Post} from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -16,7 +16,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-7f223-default-rtdb.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
       .subscribe(
         responseData => {
@@ -59,7 +62,19 @@ export class PostsService {
 
   deletePosts() {
     return this.http.delete(
-      'https://ng-complete-guide-7f223-default-rtdb.firebaseio.com/posts.json'
-    );
+      'https://ng-complete-guide-7f223-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+
+    ).pipe(tap(event=>{
+      console.log(event);
+      if (event.type === HttpEventType.Sent) {
+        // ...
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }));
   }
 }
