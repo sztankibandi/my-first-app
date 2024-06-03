b3904a8f33ae9f0b85e3fc5be6e2516a4f535038
Sztankovics András <sztankibandi@gmail.com>
2024-06-03 11:15:35 +0200

    done error handling to move it in to services

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 
 @Component({
@@ -7,7 +8,12 @@ import {NgForm} from "@angular/forms";
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-  isLoginMode: boolean = true;
+  isLoginMode:boolean = true;
+  isLoading:boolean = false;
+  error:string = null;
+
+  constructor(private authService: AuthService) {
+  }
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
@@ -15,7 +21,31 @@ export class AuthComponent {
 
 
   onSubmit(form: NgForm){
-    console.log(form.value);
-    form.reset();
+    if (!form.valid)
+    {
+      return;
+    }
+    const email: string = form.value.email;
+    const password: string = form.value.password;
+    this.isLoading = true;
+
+    if (this.isLoginMode){
+
+    }else{
+      this.authService.signup(email,password).subscribe(resData =>{
+          console.log(resData);
+          this.isLoading = false;
+        },
+        errorMessage =>{
+          console.log(errorMessage);
+          this.error = errorMessage;
+          this.isLoading = false;
+
+        });
+      form.reset();
+    }
+
+
+
   }
 }
