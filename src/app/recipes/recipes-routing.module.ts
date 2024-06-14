da59f00024b1c01c102533d819faabfe1013dee7
Sztankovics András <sztankibandi@gmail.com>
2024-06-14 11:52:30 +0200

    preloading lazy-loaded code

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,15 +1,16 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {RecipesComponent} from "./recipes.component";
-import {AuthGuard} from "../auth/auth.guard";
-import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
-import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
-import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
-import {RecipesResolverService} from "./recipes-resolver.service";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesResolverService } from './recipes-resolver.service';
 
 const routes: Routes = [
   {
-    path: 'recipes',
+    path: '',
     component: RecipesComponent,
     canActivate: [AuthGuard],
     children: [
@@ -26,7 +27,7 @@ const routes: Routes = [
         resolve: [RecipesResolverService]
       }
     ]
-  },
+  }
 ];
 
 @NgModule({
