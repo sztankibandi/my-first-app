068f23a57b610069ebe4d11cb3601cf93fdd5856
Sztankovics András <sztankibandi@gmail.com>
2024-06-17 13:55:55 +0200

    set() and update()

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  standalone: true,
+  imports: [NgFor],
+})
+export class SignalsComponent {
+  actions: string[] = [];
+  counter = signal(0);
+
+  increment() {
+    // this.counter.update((oldCounter)=>oldCounter + 1);
+    this.counter.set(this.counter() + 1);
+    this.actions.push('INCREMENT');
+  }
+
+  decrement() {
+    this.counter.update((oldCounter)=>oldCounter - 1);
+    this.actions.push('DECREMENT');
+  }
+}
