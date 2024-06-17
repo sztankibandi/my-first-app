c4f1bcca4a49584e2d46fb89316aed334274be84
Sztankovics András <sztankibandi@gmail.com>
2024-06-17 14:08:43 +0200

    finished signals

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {Component, computed, effect, signal} from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -8,17 +8,22 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
+  doubleCounter= computed(()=>this.counter()*2);
+
+  constructor() {
+    effect(() => console.log(this.counter()));
+  }
 
   increment() {
     // this.counter.update((oldCounter)=>oldCounter + 1);
     this.counter.set(this.counter() + 1);
-    this.actions.push('INCREMENT');
+    this.actions.update((oldActions)=>[...oldActions,'INCREMENT']);
   }
 
   decrement() {
     this.counter.update((oldCounter)=>oldCounter - 1);
-    this.actions.push('DECREMENT');
+    this.actions.update((oldActions)=>[...oldActions,'DECREMENT']);
   }
 }
