9423cdafc526fcf51f7b37b5d14d837527164908
Sztankovics András <sztankibandi@gmail.com>
2024-06-19 14:25:10 +0200

    NgRx start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 
-import { DefaultComponent } from './default/default.component';
-import {SignalsComponent} from "./signals/signals.component";
-
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: true,
-    imports: [DefaultComponent, SignalsComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+}
