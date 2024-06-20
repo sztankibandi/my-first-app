b8e2df401bab9c0fb1f5d25b91e0e3c9ba5dde2c
Sztankovics András <sztankibandi@gmail.com>
2024-06-20 11:13:37 +0200

    Handling Actions Without createReducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,11 +3,14 @@ import {increment} from "./counter.actions";
 
 const initialState: number = 0;
 
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value),
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value),
+// );
 
-// export function counterReducer(state = initialState) {
-//   return state;
-// }
+export function counterReducer(state = initialState, action : any = increment) {
+  if (action.type === '[Counter] Increment') {
+    return state + action.value;
+  }
+  return state;
+}
